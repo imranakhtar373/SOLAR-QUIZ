3bb1cbf72c2895105cdb985f6dce35892765f384
imranakhtar373 <imranakhtar3738@gmail.com>
2023-06-04 21:50:51 +0000

    Added number of options for following questions

--- run.py
+++ run.py
@@ -1,17 +1,28 @@
 # python quiz 
 #questions
-questions = ("1. which planet have maximum number of moons?",
-            "2. which planet named as Lucifer?",
-            "3. which is the brightest and hottest planet in the Solar System?",
-            "4. which planet has the shortest day?",
-            "5. which planet own the moons (Phobos and Deimos)?",
-            "6. what is the other name for (Orion Constellation)?",
-            "7. what is the number of known satellites of Jupiter?",
-            "8. which is the brightest star in solar system?",
-            "9. Which is the brightest star in the Orion Constellation?",
-            "10. what is the name of planet that orbits star outside of our solar system?")
+questions = ("1. which planet have maximum number of moons?: ",
+            "2. which planet named as Lucifer?: ",
+            "3. which is the brightest and hottest planet in the Solar System?: ",
+            "4. which planet has the shortest day?: ",
+            "5. which planet own the moons (Phobos and Deimos)?: ",
+            "6. what is the other name for (Orion Constellation)?: ",
+            "7. what is the number of known satellites of Jupiter?: ",
+            "8. which is the brightest star in solar system?: ",
+            "9. Which is the brightest star in the Orion Constellation?: ",
+            "10. what is the name of planet that orbits star outside of our solar system?: ")
 
-options = ((),(),(),(),(),(),(),(),(),())
+
+# options of the upper questions
+options = (("A. Saturn", "B. Venus", "C. Mercury", "D. Pluto"),
+            ("A. Neptune", "B. Venus", "C. Jupiter", "D. Uranus"),
+            ("A. Venus", "B. Saturn", "C. Sun", "D. Mars"),
+            ("A. Earth", "B. Neptune", "C. Jupiter", "D. Uranus"),
+            ("A. Saturn", "B. Jupiter", "C. Venus", "D. Mars"),
+            ("A. Hunter", "B. Taurus", "C. Ursa", "D. Oeneus"),
+            ("A. 96", "B. 80", "C. 79", "D.104"),
+            ("A. Antares", "B. Arcturus", "C. Canopus", "D. Sirius"),
+            ("A. Zeta", "B. Delta", "C. Rigel", "D. Epsilon"),
+            ("A. Outerplanets", "B. Exoplanet", "C. Gas giant", "D. Comits"))
 
 
 answers = ()

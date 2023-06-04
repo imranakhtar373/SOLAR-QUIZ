6890bfd783b36bf37934ad19618b8e20a6e63490
imranakhtar373 <imranakhtar3738@gmail.com>
2023-06-04 21:14:36 +0000

    added a tuple of ten questions

--- run.py
+++ run.py
@@ -1,6 +1,15 @@
 # python quiz 
-
-questions = ()
+#questions
+questions = ("1. which planet have maximum number of moons?",
+            "2. which planet named as Lucifer?",
+            "3. which is the brightest and hottest planet in the Solar System?",
+            "4. which planet has the shortest day?",
+            "5. which planet own the moons (Phobos and Deimos)?",
+            "6. what is the other name for (Orion Constellation)?",
+            "7. what is the number of known satellites of Jupiter?",
+            "8. which is the brightest star in solar system?",
+            "9. Which is the brightest star in the Orion Constellation?",
+            "10. what is the name of planet that orbits star outside of our solar system?")
 
 options = ((),(),(),(),(),(),(),(),(),())
 
